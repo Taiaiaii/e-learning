cc0522fa6b74e74a891ebce58026828cdcf0f2d6
Tai Taveira <taiane.taveira@luizalabs.com>
2022-05-23 12:09:50 -0300

    feat: category card component

---
+++ components/CategoryCard/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { CategoryCardContainer, CategoryContent, CategoryTitle, Lessons } from "./styles";
+
+type CategoryType = {
+  img: string ;
+  title: string;
+  lessons: string;
+};
+
+export default function CategoryCard ({img, title, lessons}: CategoryType) {
+
+    return (
+        <CategoryCardContainer>
+           <Image src={img} width={64} height={63} alt='' />
+           <CategoryContent>
+           <CategoryTitle>{title}</CategoryTitle>
+           <Lessons>{lessons} Aulas</Lessons> 
+           </CategoryContent>
+        </CategoryCardContainer>
+    )
+}
---
+++ components/CategoryCard/styles.ts
@@ -0,0 +1,37 @@
+import { styled } from '../../styles/stitches.config';
+
+export const CategoryCardContainer = styled('div', {
+    backgroundColor: '$principal',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    borderRadius: '16px',
+    width: '100%',
+    padding: '25px',
+    alignItems: 'flex-start'
+})
+
+export const CategoryContent = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingTop: '24px',
+    width: '100%',
+    
+
+})
+
+export const CategoryTitle = styled('h2', {
+    fontSize: '15px',
+    fontWeight: '400',
+    lineHeight: '20px',
+    color: '$base',
+    paddingBottom: '4px'
+})
+
+export const Lessons = styled('p', {
+    fontSize: '10px',
+    fontWeight: '400',
+    lineHeight: '12px',
+    color: '$inputs'
+
+})
