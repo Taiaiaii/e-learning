bfedc29d71aa01f7c87b8787fec480d1f554d3a0
Tai Taveira <taiane.taveira@luizalabs.com>
2022-06-01 16:56:05 -0300

    feat: creating layout to home and save courses page

---
+++ src/components/CategoryLayout/index.tsx
@@ -0,0 +1,61 @@
+import { ReactNode, useState } from 'react';
+
+import {
+  CategoryContainer,
+  Content,
+  Footer,
+  Header,
+  MainContainer,
+  Options,
+} from './styles';
+import Logo from '@public/logo.svg';
+import LogoutButton from '@public/logout.svg';
+import SearchBar from '@components/SearchBar';
+import Home from '@public/home.svg';
+import Saved from '@public/saved.svg';
+
+interface ICategoryLayoutProps {
+  children: ReactNode;
+}
+
+export default function CategoryLayout({ children }: ICategoryLayoutProps) {
+  const [isHomeSelected, setSelectHome] = useState(true);
+  const [isSavedSelected, setSelectSaved] = useState(false);
+
+  return (
+    <CategoryContainer>
+      <Header>
+        <Content>
+          <Logo />
+          <button aria-label='like'>
+            <LogoutButton />
+          </button>
+        </Content>
+        <SearchBar />
+      </Header>
+      <MainContainer>{children}</MainContainer>
+      <Footer>
+        <Options
+          selected={isHomeSelected}
+          onClick={() => {
+            setSelectHome(true);
+            setSelectSaved(false);
+          }}
+        >
+          <Home />
+          <p>Home</p>
+        </Options>
+        <Options
+          selected={isSavedSelected}
+          onClick={() => {
+            setSelectSaved(true);
+            setSelectHome(false);
+          }}
+        >
+          <Saved />
+          <p>Salvos</p>
+        </Options>
+      </Footer>
+    </CategoryContainer>
+  );
+}
