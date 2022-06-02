7ac0711089a29c61d39b1965da4f8f95a3f82b00
Tai Taveira <taiane.taveira@luizalabs.com>
2022-06-02 08:40:26 -0300

    fix: adjustments at button style

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -13,6 +13,9 @@ export const StyledButton = styled('button', {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    '& svg ': {
+        marginLeft: 10,
+    },
     variants: {
         variant: {
             filled: {
@@ -33,16 +36,10 @@ export const StyledButton = styled('button', {
         },
         iconSide: {
             right: {
-                flexDirection: 'row-reverse',
-                '& svg ': {
-                    marginLeft: 10,
-                },
+                flexDirection: 'row-reverse',               
             },
             left: {
                 flexDirection: 'row',
-                '& svg ': {
-                    marginRight: 10,
-                },
             },
 
         }
