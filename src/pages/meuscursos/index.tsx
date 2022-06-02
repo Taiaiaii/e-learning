fcce770a82af8e11fe33d8f5062fb06500103f79
Tai Taveira <taiane.taveira@luizalabs.com>
2022-06-02 09:50:38 -0300

    fix: changing component name

--- src/pages/meuscursos/index.tsx
+++ src/pages/meuscursos/index.tsx
@@ -1,7 +1,7 @@
 import { MyCoursesTemplate } from "@templates/MyCourses";
 import {MOCKED_SAVED_COURSES} from '../../../.mocks/constants/MOCKED_SAVED_COURSES'
 
-export default function MeusCursos(){
+export default function MyCourses(){
     return (
         <MyCoursesTemplate categorys={MOCKED_SAVED_COURSES}/>
     )
