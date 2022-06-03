e503a496a6502bcee3a9c763d27ef898489182ab
Tai Taveira <taiane.taveira@luizalabs.com>
2022-06-03 08:11:07 -0300

    fix: padronizing components names

--- src/pages/aulas/index.tsx
+++ src/pages/aulas/index.tsx
@@ -2,7 +2,7 @@ import { ClassesTemplate } from "@templates/Classes";
 import {MOCKED_LIST} from '../../../.mocks/constants/MOCKED_CATEGORY_LIST'
 import {MOCKED_CLASSES} from '../../../.mocks/constants/MOCKED_CLASSES'
 
-export default function Aulas (){
+export default function Classes (){
     return (
         <ClassesTemplate category={MOCKED_LIST[0]} classes={MOCKED_CLASSES}/>
     )
