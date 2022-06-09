a46943a5edc78eb9753e8ea9e50274fa127ee684
Tai Taveira <taiane.taveira@luizalabs.com>
2022-06-09 14:54:52 -0300

    fix: solving inconsistence on delete func

--- src/context/CategoriesContext/index.tsx
+++ src/context/CategoriesContext/index.tsx
@@ -1,7 +1,11 @@
 import { createContext, ReactNode, useState } from 'react';
 
-import { MOCKED_CATEGORY_LIST, MOCKED_CLASSES, MOCKED_SAVED_COURSES } from '../../../.mocks/constants'
-import { ICategory, ICourse, IClasses } from '@models'
+import {
+  MOCKED_CATEGORY_LIST,
+  MOCKED_CLASSES,
+  MOCKED_SAVED_COURSES,
+} from '../../../.mocks/constants';
+import { ICategory, ICourse, IClasses } from '@models';
 
 interface ICategoriesContextProps {
   allCategories: ICategory[];
@@ -20,40 +24,41 @@ interface ICategoriesProviderProps {
 export const CategoriesContext = createContext({} as ICategoriesContextProps);
 
 export function CategoriesProvider({ children }: ICategoriesProviderProps) {
-
   const [allCategories] = useState<ICategory[]>(MOCKED_CATEGORY_LIST);
   const [allCategoriesClasses] = useState<IClasses[]>(MOCKED_CLASSES);
-  const [savedCategories, setSavedCategories] = useState<ICategory[]>(MOCKED_SAVED_COURSES);
+  const [savedCategories, setSavedCategories] =
+    useState<ICategory[]>(MOCKED_SAVED_COURSES);
   const [classes, setClasses] = useState<ICourse[] | undefined>([]);
 
   function getCategory(id: string) {
     return allCategories.find((category) => category.id === id);
   }
 
-  function getCategoryClasses(id: string){
-    const category = getCategory(id)
-    if(category) {
+  function getCategoryClasses(id: string) {
+    const category = getCategory(id);
+    if (category) {
       const categorySelected = allCategoriesClasses.find(
         (item) => item.id === category.id
-      )
-      setClasses(categorySelected?.classes)
-      return classes
-    }   
+      );
+      setClasses(categorySelected?.classes);
+      return classes;
+    }
   }
 
   function addCategory(id: string) {
     const category = getCategory(id);
     if (category) {
-      setSavedCategories((prevState)=> ([...prevState, category]))  
+      setSavedCategories((prevState) => [...prevState, category]);
     }
-    return savedCategories
+    return savedCategories;
   }
 
   function deleteCategory(id: string) {
-    setSavedCategories(
-      savedCategories.filter((categories) => categories.id !== id)
+    const filteredSavedCategories = savedCategories.filter(
+      (categories) => categories.id !== id
     );
-    return savedCategories;
+    setSavedCategories(filteredSavedCategories);
+    return filteredSavedCategories;
   }
 
   return (
