f56505cee1f7281a2ded7930a63a2e96f718711d
Tai Taveira <taiane.taveira@luizalabs.com>
2022-06-09 08:31:14 -0300

    fix: refactor handleClick function

--- src/pages/categorias/[id].tsx
+++ src/pages/categorias/[id].tsx
@@ -12,5 +12,5 @@ export default function Classes() {
   const category = getCategory(String(id))
   const classes = getCategoryClasses(String(id));
   
-  return classes && category && <ClassesTemplate category={category} classes={classes} />
+  return category && classes && <ClassesTemplate category={category} classes={classes} />
 }
